b4eb6c57caca9dd5b35a4547b88cde387d111452
Oray Kurt <oray.kt@gmail.com>
2025-12-17 02:57:17 +0100

    trpc setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,15 @@
-import { auth } from '@/lib/auth';
-import { HomeView } from '@/pages/home/ui/views/home-view';
-import { headers } from 'next/headers';
-import { redirect } from 'next/navigation';
+import { auth } from "@/lib/auth";
+import { HomeView } from "@/pages/home/ui/views/home-view";
+import { caller } from "@/trpc/server";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
   const session = await auth.api.getSession({
     headers: await headers(),
   });
   if (!session) {
-    redirect('/sign-in');
+    redirect("/sign-in");
   }
   return <HomeView />;
 };
--- src/pages/home/ui/views/home-view.tsx
+++ src/pages/home/ui/views/home-view.tsx
@@ -1,35 +1,10 @@
-'use client';
+"use client";
 
-import { Button } from '@/components/ui/button';
-import { authClient } from '@/lib/auth-client';
-import { useRouter } from 'next/navigation';
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 
 export const HomeView = () => {
-  const router = useRouter();
-  const {
-    data: session,
-    isPending,
-    // error, //error object
-    // refetch //refetch the session
-  } = authClient.useSession();
-
-  if (isPending) {
-    return <div>Loading...</div>;
-  }
-  return (
-    <div className="flex flex-col gap-4 p-8">
-      <p>Welcome to Meet.AI. Logged in as {session?.user?.name}</p>
-      <Button
-        onClick={() =>
-          authClient.signOut({
-            fetchOptions: {
-              onSuccess: () => router.push('/sign-in'),
-            },
-          })
-        }
-      >
-        Sign out
-      </Button>
-    </div>
-  );
+  const trpc = useTRPC();
+  const { data } = useQuery(trpc.hello.queryOptions({ text: "from HomeView" }));
+  return <div className="flex flex-col gap-4 p-8">{data?.greeting}</div>;
 };
