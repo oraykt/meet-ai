f28451d817912d4608acf8eee947d92795ef66d1
Oray Kurt <oray.kt@gmail.com>
2025-12-18 09:33:15 +0100

    data-table with tanstack

--- src/pages/agents/server/procedures.ts
+++ src/pages/agents/server/procedures.ts
@@ -3,19 +3,28 @@ import { agents } from "@/db/schema";
 import { baseProcedure, createTRPCRouter, protectedProcedure } from "@/trpc/init";
 import { agentsInstanceSchema } from "../schemas";
 import { z } from "zod";
-import { eq } from "drizzle-orm";
+import { eq, getTableColumns, sql } from "drizzle-orm";
 // import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   // TODO: Change `getOne` to use protectedProcedure
   getOne: baseProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
-    const [existingAgent] = await db.select().from(agents).where(eq(agents.id, input.id));
+    const [existingAgent] = await db
+      .select({
+        ...getTableColumns(agents),
+      })
+      .from(agents)
+      .where(eq(agents.id, input.id));
     return existingAgent;
   }),
 
   // TODO: Change `getMany` to use protectedProcedure
   getMany: baseProcedure.query(async () => {
-    const data = await db.select().from(agents);
+    const data = await db
+      .select({
+        ...getTableColumns(agents),
+      })
+      .from(agents);
     // await new Promise((resolve) => setTimeout(resolve, 2000)); // Simulate delay
     // throw new TRPCError({
     //   code: "NOT_FOUND",
