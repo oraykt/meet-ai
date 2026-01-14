18c0bf87bc188e7e980045276b718ce106fb398d
Oray Kurt <17630963+oraykt@users.noreply.github.com>
2026-01-14 17:30:20 +0100

    Lint fix

--- src/app/api/inngest/route.ts
+++ src/app/api/inngest/route.ts
@@ -2,7 +2,6 @@ import { inngest } from "@/app/inngest/client";
 import { meetingsProcessing } from "@/app/inngest/functions";
 import { serve } from "inngest/next";
 
-// Create an API that serves zero functions
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [meetingsProcessing],
--- src/app/inngest/functions.ts
+++ src/app/inngest/functions.ts
@@ -39,12 +39,6 @@ export const meetingsProcessing = inngest.createFunction(
   },
   { event: "meetings/processing" },
   async ({ event, step }) => {
-    // const response = await step.fetch(event.data.transcriptUrl);
-    // const transcript = await step.run("parse transcript", async () => {
-    //   const text = await response.text();
-    //   return JSONL.parse<StreamTranscriptItem>(text);
-    // });
-
     const response = await step.run("fetch-transcript", async () => {
       return fetch(event.data.transcriptUrl).then((res) => res.text());
     });
